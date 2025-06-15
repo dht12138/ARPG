ac9e10a6d7d0fe343e1c4bc179f63a5a19bdace7
dht12138 <94498790+dht12138@users.noreply.github.com>
2025-06-15 22:47:23 +0800

    Hero StartUpData Func be updated , add HeroStartUpAbilitySer

--- Source/ARPG/Private/DataAssets/StartUpDatta/DataAsset_HeroStartUpData.cpp
+++ Source/ARPG/Private/DataAssets/StartUpDatta/DataAsset_HeroStartUpData.cpp
@@ -2,4 +2,30 @@
 
 
 #include "DataAssets/StartUpDatta/DataAsset_HeroStartUpData.h"
+#include "AbilitySystem/Abilities/ArpgGameplayAbility.h"
+#include "AbilitySystem/ArpgAbilitySystemComponent.h"
 
+bool FArpgHeroAbilitySet::IsValid() const
+{
+    return InputTag.IsValid() && AbilityToGrant;
+}
+
+void UDataAsset_HeroStartUpData::GiveToAbilitySystemComponent(UArpgAbilitySystemComponent* InASCToGive,
+    int32 ApplyLevel)
+{
+    Super::GiveToAbilitySystemComponent(InASCToGive, ApplyLevel);
+
+    for (const FArpgHeroAbilitySet& AbililtySet : HeroStartUpAbilitySet)
+    {
+        if (!AbililtySet.IsValid()) continue;
+
+        FGameplayAbilitySpec AbilitySpec(AbililtySet.AbilityToGrant);
+        AbilitySpec.SourceObject = InASCToGive->GetAvatarActor();
+        AbilitySpec.Level = ApplyLevel;
+        AbilitySpec.DynamicAbilityTags.AddTag(AbililtySet.InputTag);
+        //使用DynamicAbilityTags 对GameplayAbility使用InputTag进行标记，可以使用该InputTag检索该GameplayAbility
+
+        InASCToGive->GiveAbility(AbilitySpec);
+    }
+
+}
--- Source/ARPG/Public/DataAssets/StartUpDatta/DataAsset_HeroStartUpData.h
+++ Source/ARPG/Public/DataAssets/StartUpDatta/DataAsset_HeroStartUpData.h
@@ -4,7 +4,23 @@
 
 #include "CoreMinimal.h"
 #include "DataAssets/StartUpDatta/DataAsset_StartUpDataBasee.h"
+#include "GameplayTagContainer.h"
 #include "DataAsset_HeroStartUpData.generated.h"
+
+USTRUCT(BlueprintType)
+struct FArpgHeroAbilitySet
+{
+	GENERATED_BODY()
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly, meta = (Categories = "InputTag"))
+	FGameplayTag InputTag;
+
+	UPROPERTY(EditDefaultsOnly, BlueprintReadOnly)
+	TSubclassOf<UArpgGameplayAbility> AbilityToGrant;
+
+	bool IsValid() const;
+
+};
 
 /**
  * 
@@ -12,6 +28,13 @@
 UCLASS()
 class ARPG_API UDataAsset_HeroStartUpData : public UDataAsset_StartUpDataBasee
 {
-	GENERATED_BODY()
+	GENERATED_BODY()  
 	
+public:
+	virtual void GiveToAbilitySystemComponent(UArpgAbilitySystemComponent* InASCToGive, 
+		int32 ApplyLevel = 1) override;
+
+private:
+	UPROPERTY(EditDefaultsOnly, Category = "StartUpData", meta = (TitleProperty = "InputTag"))
+	TArray<FArpgHeroAbilitySet> HeroStartUpAbilitySet;
 };
